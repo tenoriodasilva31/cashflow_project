1ee7b3dd7b51e01e38ad75ab4fe4bc261bef1a7a
Jonathan Tenório da Silva <jonathan.tenoriodev@gmail.com>
2023-08-12 16:09:29 -0300

    project first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cashflow_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
